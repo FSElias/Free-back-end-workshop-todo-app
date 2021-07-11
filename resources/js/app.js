d9d75bcfd30bb7a8f1f1c8241c88eb84a90f2ebe
Fady Elias <fselias@gmail.com>
2021-07-11 15:45:43 +0200

    Update task input form

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 
 require('./bootstrap');
 
-import vue from 'vue'
-
 import App from './vue/app';
+
+
 const app = new Vue({
     el: '#app',
     components: {App}
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,10 +1,45 @@
 <template>
-    <div>
-        Hi There
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title"> Todo List</h2>
+            <add-task-form/>
+        </div>
+        <list-view/>
     </div>
 </template>
 <script>
-export default {
+import AddTaskForm from "./AddTaskForm";
+import ListView from "./ListView";
 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faPlusSquare, faTrash} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import Vue from "vue";
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+export default {
+    components: {
+        AddTaskForm,
+        ListView
+    }
 }
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>
